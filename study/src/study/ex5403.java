fe4e8c4f29d8f126b121928ab47b59dcd700e647
=yeinhan <goregore8637@naver.com>
2020-07-29 16:56:52 +0900

    no message

--- study/src/study/ex5403.java
+++ study/src/study/ex5403.java
@@ -1,94 +1,62 @@
 package study;
 
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
 
-import javax.sound.midi.Soundbank;
-
-public class ex5403 {
+public class ex5403 extends Exception {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
-		int size;
-		System.out.print("사이즈 입력:");
-		size = sc.nextInt();
-		
-//		int a[] = new int[size];
-//		
-//		for(int i=0; i<size; i++) {
-//			a[i] = sc.nextInt();
-//			
-//		}
-//		
-//		
-//		R(a,size);
-//		
-//		for(int i=0; i<size; i++) {
-//			System.out.printf("%d  ",a[i]);
-//		}
-//		System.out.println();
-//		
-//		D(a,size);
-//		
-//		for(int i=0; i<size; i++) {
-//			System.out.printf("%d  ",a[i]);
-//		}
-		ArrayList<Integer> a1 = new ArrayList<Integer>(size);
-		
-		for(int i=0; i<size; i++) {
-			a1.add(sc.nextInt());
-			
+		ArrayList<Integer> a = null;
+		int size1;
+		size1 = sc.nextInt();
+
+		for (int i = 0; i < size1; i++) {
+			int size;
+
+			String str = sc.next();
+
+			String[] strArray;
+			strArray = str.split("");
+
+			size = sc.nextInt();
+
+			a= new ArrayList<Integer>(size);
+			try {
+				for (int k = 0; k < size; k++) {
+					a.add(sc.nextInt());
+				}
+				
+				for (int j = 0; j < strArray.length; j++) {
+					if (strArray[j].equals("R")) {
+						a = R(a, size);
+					} else if (strArray[j].equals("D")) {
+						a = D(a, size);
+					}
+				}
+				
+				System.out.println(a);
+				
+			} catch (Exception e) {
+				System.out.println("error");
+			}
+
 		}
-		
-		
-		R(a1,size);
-		
-		
-		D(a1,size);
-		
-		R(a1,size);
-		
+//		System.out.println(a);
 	}
-	
-	
-	public static void R(ArrayList<Integer> a1, int size) {
-//		int temp;
-//		
-//		for(int i=0; i<size/2; i++) {
-//			temp = a[i];
-//			a[i] = a[(size-1)-i];
-//			a[(size-1)-i] = temp;
-//			
-//		}
-		Collections.reverse(a1);
-		System.out.println(a1);
-		
-		
+
+	public static ArrayList<Integer> R(ArrayList<Integer> a, int size) {
+		Collections.reverse(a);
+		return a;
+
 	}
-	
-	public static void D(ArrayList<Integer> a1, int size) {
-//		for(int i=0; i<size-1; i++) {
-//			a[i] = a[i+1];
-//		}
-		
-//		int cnt = 0;
-//		Scanner sc = new Scanner(System.in);
-//		
-//		while(true) {
-//		        for(int i=0; i<size-1; i++) {
-//		             a[i] = a[i + 1];
-//		        }
-//		        size = size - 1;
-//		}
-		
-		a1.remove(1);
-		System.out.println(a1);
-		
+
+	public static ArrayList<Integer> D(ArrayList<Integer> a, int size) {
+		a.remove(0);
+		return a;
+
 	}
-  
+
 }
