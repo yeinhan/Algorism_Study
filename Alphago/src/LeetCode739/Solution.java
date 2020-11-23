0ac348ad8acdf3716f11f65e278f5c902fd105d2
=yeinhan <goregore8637@naver.com>
2020-11-24 00:04:33 +0900

    LeetCode 739

---
+++ Alphago/src/LeetCode739/Solution.java
@@ -0,0 +1,40 @@
+package LeetCode739;
+
+import java.util.Arrays;
+
+/*
+ * Runtime: 1085ms
+ * Memory Usage: 47.2MB
+ * 
+ * */
+
+public class Solution {
+
+	public int[] dailyTemperatures(int[] T) {
+		int count = 0;
+		int[] anw = new int[T.length];
+		
+		for(int i=0; i<T.length; i++) {
+			count = 0;
+			for(int j=i+1; j<T.length; j++) {
+				count += 1;
+				if(T[i] < T[j]) {	//핵심
+					anw[i] = count;
+					break;
+				}
+			}
+			
+		}
+		
+		return anw;
+
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int s[] = {73, 74, 75, 71, 69, 72, 76, 73};
+		int[] asw = solution.dailyTemperatures(s);
+		
+		System.out.println(Arrays.toString(asw));
+	}
+}
