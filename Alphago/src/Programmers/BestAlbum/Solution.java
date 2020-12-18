f39882c926fd5680ea3a1e119805c18a88b2cbbd
=yeinhan <goregore8637@naver.com>
2020-12-18 22:42:36 +0900

    29번 못풀음,,

---
+++ Alphago/src/Programmers/BestAlbum/Solution.java
@@ -0,0 +1,22 @@
+package Programmers.BestAlbum;
+
+import java.util.Hashtable;
+
+public class Solution {
+	public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        
+        for(int i=0; i<genres.length; i++) {
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		Hashtable<String, Integer> hash = new Hashtable<String, Integer>();
+		hash.put("cla", 500);
+		hash.put("cla", 600);
+		
+		System.out.println(hash);
+	}
+}
