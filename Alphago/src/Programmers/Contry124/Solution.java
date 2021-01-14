6b4b9072ba3cb77088c4116441f716b7eddc2c0d
=yeinhan <goregore8637@naver.com>
2021-01-14 22:25:52 +0900

    알파고 35번

---
+++ Alphago/src/Programmers/Contry124/Solution.java
@@ -0,0 +1,34 @@
+package Programmers.Contry124;
+
+public class Solution {
+	public String solution(int n) {
+		String answer = "";
+
+		int temp = 0;
+
+		while (n > 0) {
+			temp = n % 3;
+			n = n / 3;
+
+			if (temp == 0) {
+				temp = 4;
+				n -= 1;
+			}
+
+			System.out.println("n:" + n + " temp:" + temp + " ans:" + answer + "===" + temp + answer);
+			answer = temp + answer;
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+
+		Solution s = new Solution();
+//		for(int i=1; i<=10; i++) {(i));
+//			
+//			System.out.println("i:"+i +" re:"+s.solution
+//		}
+		System.out.println(s.solution(10));
+	}
+}
